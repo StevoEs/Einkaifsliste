db5c11590c935e5141abad14e7c9f4505ddc3b69
StevoEs <stefan-studenski@live.de>
2022-10-20 12:39:53 +0200

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 export default defineConfig({
+    css: {
+        modules: {
+          scopeBehaviour: 'global',
+        },
+      },
     root: "src",
     assetsInclude: ["./assets/**/*.*"],
     build: {
